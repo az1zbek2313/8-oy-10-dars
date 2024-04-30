b9d87868e71a11b5974f10b2f0e24c25cce83bd7
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-30 22:01:42 +0500

    biroz o'zgarishlar kiritildi

--- src/Example/Example.jsx
+++ src/Example/Example.jsx
@@ -40,8 +40,6 @@ function Exaple() {
     }
   }
 
-  console.log(25, shablonRef);
-  console.log(26, textRef);
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between title">
@@ -221,7 +219,7 @@ function Exaple() {
           <div>
             <a
               href="#"
-              className={`linkMessage block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${
+              className={`linkMessage block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${
                 shablonRef == 10 && "bg-gray-200" ||
                 shablonRef == 20 && "bg-blue-200" ||
                 shablonRef == 30 && "bg-yellow-200" 
